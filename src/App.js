94e805b6f4dd1040a070e2a053a259b83bf5ec2c
Wilson Beda <wilsontrovaobeda@gmail.com>
2022-12-12 14:06:44 -0300

    form, submit, useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
-import Name from './components/Name'
+import { useState } from 'react';
 
 function App() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [idade, setIdade] = useState('');
+
+  const [user, setUser] = useState({});
+
+  function handleRegister(e){
+    // bloqueia a atualização automática da página
+    e.preventDefault();
+
+    alert("Usuário registrado com sucesso!")
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email
+    })
+  }
+
   return (
     <div>
-      <h1></h1>
-      <Name aluno="Wilson" idade="15"/>
+      <h1> Cadastro de Usuário: </h1>
+      <form onSubmit={handleRegister}>
+      <label>Nome:</label><br/>
+      <input placeholder='Digite seu Nome' 
+      value = {nome}
+      onChange = { (e) => setNome(e.target.value)}
+      /><br/>
+
+      <label>Email:</label><br/>
+      <input placeholder='Digite seu Email'
+       value = {email}
+       onChange = { (e) => setEmail(e.target.value)}
+      /><br/>
+
+      <label>Idade:</label><br/>
+      <input placeholder='Digite sua Idade'
+       value = {idade}
+       onChange = { (e) => setIdade(e.target.value)}
+      /><br/>
+
+      <button type="submit"> Registrar </button>
+      </form>
+      <br/><br/>
+      <div>
+        <span>Bem vindo: {user.nome}</span> <br/>
+        <span>Idade: {user.idade}</span> <br/>
+        <span>Email: {user.email}</span> <br/>
+      </div>
     </div>
   );
 }
