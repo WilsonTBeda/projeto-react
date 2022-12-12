9967349ab8948d30381534e65a2cc37d4460c024
Wilson Beda <wilsontrovaobeda@gmail.com>
2022-12-12 13:43:07 -0300

    Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Name from './components/Name'
+
 function App() {
   return (
     <div>
-      <h1> Hello World </h1>
+      <h1></h1>
+      <Name aluno="Wilson" idade="15"/>
     </div>
   );
 }
---
+++ src/components/Name.js
@@ -0,0 +1,8 @@
+function Name({ aluno, idade }){
+    
+    return(
+    <span>Seja bem vindo: {aluno} -  Idade: {idade} anos </span>
+    )
+}
+
+export default Name;
